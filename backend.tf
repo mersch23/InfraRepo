59faf61b2bfbb313de47f747b603185aae7c2456
mersvh <107088963+mersch23@users.noreply.github.com>
2023-02-14 16:54:21 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "mersch-aws-tf-state-bucket"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "mersch-dynamo-db-table"
   }
 }
